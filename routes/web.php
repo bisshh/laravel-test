5f8c241a60d640d9fb3f3ebe51144dfd3de5fcc5
Resham <reshamkhadka61@gmail.com>
2018-12-29 09:40:21 +0545

    creating resource method for customer crud url

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('/customer', 'CustomerController@index');
+Route::resource('/customer', 'CustomerController');
